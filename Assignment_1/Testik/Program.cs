673aaee51f9ea1b736a16d5e21eeefe56badf9f6
Nastya <nasteagaydarly@gmail.com>
2025-04-07 00:22:54 +0300

    Add a cry from the heart from Nastiuskin

--- Assignment_1/Testik/Program.cs
+++ Assignment_1/Testik/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World from Nastya!");
+Console.WriteLine("Hello, World from Nastiuskin!");
+Console.WriteLine("Nastiuskin is the happiest girl ever");
